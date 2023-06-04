4bb911a7a9683c7de9c78b71895c4b63cee81b53
Vaishali <vaishalisharmaukl@gmail.com>
2023-06-04 15:32:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+//   // Function to handle the document load event
+//   function handleDocumentLoad() {
+//     var navbar = document.querySelector('.navbar');
+
+//     // Add the class to trigger the animation after 1 second
+//     setTimeout(function() {
+//       navbar.classList.add('slide-down');
+//     }, 1000);
+//   }
+
+//   // Add document load event listener
+//   document.addEventListener('DOMContentLoaded', handleDocumentLoad);
+
+
+
+
+//   // Function to handle the document load event
+//   function handleDocumentLoad() {
+//     var navbar = document.querySelector('.navbar');
+//     var heading = document.querySelector('.heading');
+//     var paragraph = document.querySelector('.paragraph');
+
+//     // Add the class to trigger the navbar animation after 1 second
+//     setTimeout(function() {
+//       navbar.classList.add('slide-down');
+//     }, 1000);
+
+//     // Add the class to trigger the heading and paragraph animation after 2 seconds
+//     setTimeout(function() {
+//       heading.classList.add('slide-up');
+//       paragraph.classList.add('slide-up');
+//     }, 2000);
+//   }
+
+//   // Add document load event listener
+//   document.addEventListener('DOMContentLoaded', handleDocumentLoad);
