d61ca3b5ff8335f205f47c2c8d9a1796fa8a76a9
Vaishali <vaishalisharmaukl@gmail.com>
2023-07-08 00:28:12 +0530

    updated portfolio

--- script.js
+++ script.js
@@ -35,3 +35,30 @@
 
 //   // Add document load event listener
 //   document.addEventListener('DOMContentLoaded', handleDocumentLoad);
+document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll('.card');
+  
+    cards.forEach(function(card) {
+      card.addEventListener('click', function() {
+        this.classList.toggle('active');
+      });
+  
+      card.addEventListener('mouseleave', function() {
+        this.classList.remove('active');
+      });
+    });
+  });
+  
+  window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    var offset = navbar.offsetHeight;
+  
+    if (scrolled > offset) {
+      navbar.classList.add('navbar-scrolled');
+    } else {
+      navbar.classList.remove('navbar-scrolled');
+    }
+  });
+  
+  
